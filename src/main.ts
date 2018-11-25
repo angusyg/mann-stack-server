b590788964e5e791ffb3d7020debc211632d5103
angusyg <gael.clair@gmail.com>
2018-11-25 16:43:58 +0100

    Add of logger module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { LogService } from './app/logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: false });
+  app.useLogger(app.get<LogService>(LogService));
   await app.listen(3000);
 }
 bootstrap();
