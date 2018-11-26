fd2f02b20c12838ca748d2e76131f32e5b9081fe
angusyg <gael.clair@gmail.com>
2018-11-27 00:35:29 +0100

    Add of CORS configuration values

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -42,12 +42,28 @@ export class ConfigService {
       .valid(['development', 'production', 'test'])
       .default('development'),
     PORT: Joi.number().default(3000),
-    DB_HOST: Joi.string().default('localhost'),
+    DB_HOST: Joi.string().default('localhost:27017'),
     DB_NAME: Joi.string().default('mann'),
     LOG_LEVEL: Joi.string()
       .valid(['trace', 'debug', 'info', 'warn', 'error', 'fatal'])
       .default('info'),
-    LOG_ENABLED: Joi.boolean().default(false),
+    LOG_ENABLED: Joi.boolean().default(true),
+    CORS_ALLOWED_ORIGINS: Joi.array()
+      .items(Joi.string())
+      .default([]),
+    CORS_ALLOWED_METHODS: Joi.array()
+      .items(Joi.string().valid(['HEAD', 'GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE']))
+      .default(['HEAD', 'GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE']),
+    CORS_ALLOWED_HEADERS: Joi.array()
+      .items(Joi.string())
+      .default(['Authorization', 'Refresh', 'Content-type']),
+    CORS_EXPOSED_HEADERS: Joi.array()
+      .items(Joi.string())
+      .default([]),
+    CORS_CREDENTIALS: Joi.boolean().default(true),
+    CORS_MAX_AGE: Joi.number().default(600),
+    CORS_PREFLIGHT_CONTINUE: Joi.boolean().default(false),
+    CORS_OPTIONS_SUCCESSS_CODE: Joi.number().default(204),
   });
 
   /**
