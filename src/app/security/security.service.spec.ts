8b626004a5e2b6e269574d9d22f8df6cc40d2d44
angusyg <gael.clair@gmail.com>
2018-11-27 00:34:44 +0100

    Add of security module

---
+++ src/app/security/security.service.spec.ts
@@ -0,0 +1,17 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { SecurityService } from './security.service';
+
+describe('SecurityService', () => {
+  let service: SecurityService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SecurityService],
+    }).compile();
+    service = module.get<SecurityService>(SecurityService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
